ee03e68aac6a294710ebebff657e1ad1304df712
kaz-ozy <132529533+kaz-ozy@users.noreply.github.com>
2023-06-04 15:31:22 +0900

    変更しました

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,7 +1,7 @@
 class CommentsController < ApplicationController
 
   def create
-    Comment.create(comment_params)
+    @comment = Comment.new(comment_params)
     if @comment.save
     redirect_to prototype_path(@comment.protype)
     else
@@ -17,4 +17,5 @@ class CommentsController < ApplicationController
   def comment_params
   params.require(:comment).permit(:text).merge(user_id: current_user.id, prototype_id: params[:prototype_id])
   end
-end  
+
+end 
--- app/models/comment.rb
+++ app/models/comment.rb
@@ -3,6 +3,6 @@ class Comment < ApplicationRecord
   belongs_to :prototype
 
 
-  validates :content, presence: true
+  validates :text, presence: true
   validates :text, presence: true
 end
